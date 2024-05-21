9e9a4289cfddf420d7da40b324f96ecb0e5d19ca
SHAIK MANSOOR <mansoorshaik2621@gmail.com>
2024-05-21 19:26:28 +0530

    All html , css and js are changed sucessfully

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ function changeHeading() {
     heading.textContent = 'Heading Changed!';
     heading.style.color = '#d32f2f';
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const navLinks = document.getElementById('nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        burger.classList.toggle('toggle');
+    });
+});
